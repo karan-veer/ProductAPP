735e3356e9682117a14c07998c2aa5c91a2e798a
unknown <socgend42@iiht.tech>
2020-07-09 04:02:37 +0530

    First Commit

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> list = new List<Product>()
+        {
+            new Product() { Pid=1,Pname="Earphones",Price=50,Stock=10 }
+        };
+
+        public IActionResult Index()
+        {
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                list.Add(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Create");
+            }
+        }
+
+
+    }
+}
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage ="Id Required")]
+        public int Pid { get; set; }
+        [Required(ErrorMessage = "Name Required")]
+        [RegularExpression("[a-zA-Z]{3}",ErrorMessage ="Invalid Name") ]
+        public string Pname { get; set; }
+        [Required(ErrorMessage = "Price Value Required")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Stock value Required")]
+        public int Stock { get; set; }
+
+
+    }
+}
